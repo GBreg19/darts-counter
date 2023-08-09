b8abcd6041a50ae25a0e1781de056d43e9776787
Giorgi Bregvadze <giorgi.bregvadze.1998@gmail.com>
2023-08-09 13:17:12 +0400

    table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,29 +1,39 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { DartContext } from "../store/dart-context";
 import Input from "../layout/Input";
+import { LuRefreshCcw } from "react-icons/lu";
 
 const Table = () => {
   const DartCtx = useContext(DartContext);
 
   return (
     <div className="w-1/2 m-auto bg-white rounded-md absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 py-5">
-      <h1 className="text-4xl text-center">Let's Play!</h1>
+      <div className="relative">
+        <h1 className="text-4xl text-center">Let's Play!</h1>
+        <button
+          className="absolute top-1/2 -translate-y-1/2 right-10 text-xl"
+          title="Refresh"
+          onClick={() => DartCtx.setIsSubmitted(false)}
+        >
+          <LuRefreshCcw />
+        </button>
+      </div>
       <div
         className={`w-full mt-5 grid grid-cols-${DartCtx.players.length} justify-between px-5`}
       >
-        {Array.from({ length: DartCtx.playerQuantity }, (_, index) => (
+        {Array.from({ length: DartCtx.players.length }, (_, index) => (
           <div key={index} className="w-full">
             <div>
-              {Object.values(DartCtx.playerNames).map((name, i) =>
+              {DartCtx.players.map((player, i) =>
                 index === i ? (
-                  <h1 key={i} className="text-2xl capitalize font-medium bg-gray-200 p-2 text-center">
-                    {name} ({DartCtx.maxScore})
-                  </h1>
+                  <Fragment key={i}>
+                    <h1 className="text-2xl capitalize font-medium bg-gray-200 p-2 text-center">
+                      {player.name} ({player.score})
+                    </h1>
+                    <Input id={i} />
+                  </Fragment>
                 ) : null
               )}
-            </div>
-            <div>
-              <Input />
             </div>
           </div>
         ))}
