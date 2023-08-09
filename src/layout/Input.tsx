ba27b3ae8615f0e3da1bde0499aca6a9cf35b8d8
Giorgi Bregvadze <giorgi.bregvadze.1998@gmail.com>
2023-08-09 13:17:07 +0400

    updating

--- src/layout/Input.tsx
+++ src/layout/Input.tsx
@@ -1,5 +1,27 @@
-const Input = () => {
-  return <input type="number" name="scoreInp" id="scoreInp" placeholder="0" className="appearance-none w-full border px-2 py-1 focus:border-dartGreen1 focus:outline-none focus:shadow-outline" />;
+import { useContext } from "react";
+import { DartContext } from "../store/dart-context";
+
+type InpProps = {
+  id: number;
+};
+
+const Input = ({ id }: InpProps) => {
+  const DartCtx = useContext(DartContext);
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+  };
+
+  return (
+    <input
+      type="number"
+      name="scoreInp"
+      id="scoreInp"
+      placeholder="0"
+      className="appearance-none w-full border px-2 py-1 focus:border-dartGreen1 focus:outline-none focus:shadow-outline"
+      onChange={onInputChange}
+    />
+  );
 };
 
 export default Input;
